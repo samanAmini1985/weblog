d567ae800429ab49e8e332a6a004de68f13a05c5
samanAmini1985 <samaning1@gmail.com>
2023-04-07 17:30:20 +0330

    Phase1

--- weblog/settings.py
+++ weblog/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -100,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'Asia/Tehran'
 
