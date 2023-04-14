3daaf8184f83424d1e3628e710330691e5b3dd0c
samanAmini <samaning1@gmail.com>
2023-04-14 22:24:38 +0330

    phase 5

--- weblog/settings.py
+++ weblog/settings.py
@@ -99,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'fa-ir'
+LANGUAGE_CODE = 'en-uk'
 
 TIME_ZONE = 'Asia/Tehran'
 
